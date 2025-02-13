f5793765e558ea9efcb6848b985aaf454f28a2d3
José Romero <romeromanuel@unah.hn>
2025-02-12 20:19:57 -0600

    Arreglo puerto

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.DB_PORT;
+const port = process.env.DB_PORT || 3001;
 const app = express();
 app.use(express.json());
 
@@ -19,6 +19,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
